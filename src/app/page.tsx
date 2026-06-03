4266cf69d276852d29b5a90389765ff6f5bf78e1
azizjalal951-cpu <azizjalal951@gmail.com>
2026-06-03 11:38:05 +0800

    tes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
   return (
     <div className="min-h-screen bg-luxury-dark overflow-x-hidden">
       <main className="bg-luxury-dark text-luxury-light">
-        <Hero />
+        {/*<Hero />*/}
         <About />
         <Skills />
         <Projects />
